f9839c211e7a7dbb4c1b87e5e43a9d81dc9c6932
gashnick <gashnicolas@gmail.com>
2024-09-05 14:36:18 +0200

    aupdate about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
-import format from "../../assets/format.png";
+import profile from "../../assets/profile.webp";
 import "./about.css";
 export default function About() {
   const percentage = 65;
@@ -10,74 +10,68 @@ export default function About() {
 
   return (
     <section className="bg-pink-50">
-      <div className="content">
-        <div className="about-content">
-          <div className="about-image">
-            <img src={format} className="" />
+      <div className="about-content">
+        <div className="about-image">
+          <img src={profile} />
+        </div>
+        <div className="about-description">
+          <h2 className="text-red-500">ABOUT ME</h2>
+          <h3 className="text-3xl">
+            I Can Develop Anything <br /> For Your Needs
+          </h3>
+          <div className="about-description-percentage">
+            <div className="w-16">
+              <CircularProgressbar
+                value={percentage1}
+                text={`${percentage1}%`}
+                styles={buildStyles({
+                  textColor: "#EF4444",
+                  pathColor: "#EF4444",
+                  trailColor: "#FEE2E2",
+                })}
+              />
+            </div>
+            <p className="text-gray-600">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              <br />
+              Aut qui hic atque tenetur
+            </p>
           </div>
-          <div className="about-contents">
-            <div className="gap-4 m-4">
-              <h2 className="items-center text-red-500 uppercase"> About me</h2>
-              <h3 className="text-3xl capitalize mb-8">
-                I can develop anything <br />{" "}
-                <span className="inline-block">for your needs</span>
-              </h3>
+          <div className="about-description-percentage">
+            <div className="w-16">
+              <CircularProgressbar
+                value={percentage}
+                text={`${percentage}%`}
+                styles={buildStyles({
+                  textColor: "#EF4444",
+                  pathColor: "#EF4444",
+                  trailColor: "#FEE2E2",
+                })}
+              />
             </div>
-
-            <div className="flex items-center gap-4 m-4">
-              <div className="w-16">
-                <CircularProgressbar
-                  value={percentage1}
-                  text={`${percentage1}%`}
-                  styles={buildStyles({
-                    textColor: "#EF4444",
-                    pathColor: "#EF4444",
-                    trailColor: "#FEE2E2",
-                  })}
-                />
-              </div>
-              <p className="text-gray-600">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                <br />
-                <span className="inline-block">Aut qui hic atque tenetur.</span>
-              </p>
+            <p className="text-gray-600">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              <br />
+              Aut qui hic atque tenetur
+            </p>
+          </div>
+          <div className="about-description-percentage">
+            <div className="w-16">
+              <CircularProgressbar
+                value={percentage2}
+                text={`${percentage2}%`}
+                styles={buildStyles({
+                  textColor: "#EF4444",
+                  pathColor: "#EF4444",
+                  trailColor: "#FEE2E2",
+                })}
+              />
             </div>
-            <div className="flex items-center gap-4 m-4">
-              <div className="w-16">
-                <CircularProgressbar
-                  value={percentage}
-                  text={`${percentage}%`}
-                  styles={buildStyles({
-                    textColor: "#EF4444",
-                    pathColor: "#EF4444",
-                    trailColor: "#FEE2E2",
-                  })}
-                />
-              </div>
-              <p className="text-gray-600">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                <br />
-                <span className="inline-block">Aut qui hic atque tenetur.</span>
-              </p>
-            </div>
-            <div className="flex items-center gap-4 m-4">
-              <div className="w-16">
-                <CircularProgressbar
-                  value={percentage2}
-                  text={`${percentage2}%`}
-                  styles={buildStyles({
-                    textColor: "#EF4444",
-                    pathColor: "#EF4444",
-                    trailColor: "#FEE2E2",
-                  })}
-                />
-              </div>
-              <p className="text-gray-600">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                <br />
-                <span className="inline-block">Aut qui hic atque tenetur.</span>
-              </p>
-            </div>
+            <p className="text-gray-600">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              <br />
+              Aut qui hic atque tenetur
+            </p>
           </div>
         </div>
       </div>
